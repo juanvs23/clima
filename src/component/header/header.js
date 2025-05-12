b5f30c8d121ea0dc2d50ef0768f0916440eac695
Juan Carlos Avila <juanvs23@gmail.com>
2025-05-12 09:53:04 -0400

    inicio del proyecto

--- src/component/header/header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-
-const Header = (props) => {
-    return (<header  ><nav className="nav-wrapper light-blue darken-2" ><a  href = "#top" className="brand-logo">{props.title}</a></nav></header>)
-}
-export default Header
